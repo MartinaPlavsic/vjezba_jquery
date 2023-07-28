598ae9f1d4c79183323e604a9bfbd41a0c47fe5d
MARTINA PLAVŠIĆ <martina.plavsic99@gmail.com>
2023-07-28 20:17:30 +0200

    Refactoring

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-
-(async function () {
-const source = document.getElementById("entry-template").innerHTML;
-const template = Handlebars.compile(source);
-const response = await fetch("https://pokeapi.co/api/v2/pokemon-color/yellow")
-const data = await response.json()
-const pokemons = data.pokemon_species.slice(0,20)
-const context = { pokemons};
-const html = template(context);
-
-document.querySelector("body").innerHTML=html
-
-}) ()
-        
+$(document).ready(async function() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon-color/yellow"
+      );
+      const data = await response.json();
+      const source = $("#pokemon-list").html();
+      const template = Handlebars.compile(source);
+      const context = {
+        pokemon: data.pokemon_species.slice(0, 20),
+        title: "Pokemons",
+      };
+      const html = template(context);
+  
+      $("#result").html(html);
+    } catch (error) {
+      console.log(error);
+    }
+  });
